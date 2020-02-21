733261bb1c7c2687957b15897ac8e52c2e3d8767
Felipe Moraes <felipe@anank.com.br>
2020-02-20 22:32:57 -0300

    Inicio da estruturação da aplicação em PHP puro

--- PHP Estruturado/connections/connection.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-$conn = new mysqli('127.0.0.1:3600', 'root', 'secret', 'mnteste');
-
-if ($conn->connect_errno) {
-    die('Falhou em conectar: (' . $conn->connect_errno . ') ' . $conn->connect_error);
-}
